568ef8443065835b729b2dffe19c7b2b604e96bc
brooks richard <brooksrichard029@gmail.com>
2022-10-18 20:42:33 -0500

    List of objectives

--- src/index.js
+++ src/index.js
@@ -7,3 +7,28 @@ const App = () => {
 }
 
 ReactDOM.render(<App />, document.getElementById("app"))
+
+/*
+User:
+-POST /api/users/register
+-POST /api/users/login
+-GET /api/users/me
+-GET /api/users/:username/routines
+
+Activities:
+-GET /api/activities
+-POST /api/activities (*)
+-PATCH /api/activities/:activityId (*)
+-GET /api/activities/:activityId/routines
+
+Routines:
+-GET /api/routines
+-POST /api/routines (*)
+-PATCH /api/routines/:routineId (**)
+-DELETE /api/routines/:routineId (**)
+-POST /api/routines/:routineId/activities
+
+Routine-activities:
+-PATCH /api/routine_activities/:routineActivityId (**)
+-DELETE /api/routine_activities/:routineActivityId (**)
+*/
