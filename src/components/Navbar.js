5c628dc6898d548f1d11fca99bca1abee31e0946
Samuel <samuel.koch08@gmail.com>
2022-10-30 20:53:11 -0600

    css changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-const Navbar = ({ logout }) => {
+const Navbar = ({ logout, token }) => {
+
+    const linkStyle = {
+        margin: '1.5rem',
+        fontFamily: 'Staatliches',
+        fontSize: '1.5rem',
+        textDecoration: 'none',
+        color: 'white'
+    };
+
     return (
         <header>
             <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/routines'>Routines</Link>
-                <Link to='/activities'>Activities</Link>
-                <Link to='/profile'>Profile</Link>
-                <Link to='/register'>Register</Link>
-                <Link to='/login'>Login</Link>
-                <Link to='/' onClick={ () => logout() }>Logout</Link>
+                <Link to='/' style={linkStyle}>Home</Link>
+                <Link to='/routines' style={linkStyle}>Routines</Link>
+                <Link to='/activities' style={linkStyle}>Activities</Link>
+                <Link to='/profile' style={linkStyle}>Profile</Link>
+                <Link to='/register' style={linkStyle}>Register</Link>
+                <Link to='/login' style={linkStyle}>Login</Link>
+                <Link to='/' style={linkStyle} onClick={() => logout()}>Logout</Link>
             </nav>
         </header>
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Route, BrowserRouter, Routes, useNavigate } from 'react-router-dom';
-// import './style.css'
+import CssBaseline from '@mui/material/CssBaseline';
+import './style.css'
 import {
     Navbar,
     Routines,
@@ -113,9 +114,11 @@ const App = () => {
 const container = document.querySelector('#app')
 const root = ReactDOM.createRoot(container);
 root.render(
+    <CssBaseline>
     <BrowserRouter>
         <App />
     </BrowserRouter>
+    </CssBaseline>
 );
 
 /*
